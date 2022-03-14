427becfc335b7ca8f69f30837bc53b4ff1e65677
Sushmitanidhi1502 <73749602+Sushmitanidhi1502@users.noreply.github.com>
2022-03-14 21:55:09 +0530

    Add files via upload

---
+++ leapyr.java
@@ -0,0 +1,25 @@
+
+public class leapyr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=2000;
+		//if(year%400==0)
+		//{
+			//System.out.println("Its a leapyear\n");
+		//}
+		//else if(year%100==0)
+		//{
+		//System.out.println("Its not a leap year\n")	
+		//}
+		if(year%4==0) {
+			System.out.println("Its a leapyear\n");
+			}
+		else  {
+			System.out.println("Its not a leap year\n")	;
+			}
+			
+		}
+	}
+
+
